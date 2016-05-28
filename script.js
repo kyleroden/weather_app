ec6f1a8017fb06d126aba1d09ad754d60bc57b9d
kyleroden <kyleroden77@gmail.com>
2016-05-27 23:15:31 -0700

    added css, added windspeed, added farenheit temps

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ function show_weather() {
     var lat = Math.floor(data.lat);
     var long = Math.floor(data.lon);
     var display_city = document.getElementById("city_display");
-    display_city.innerHTML = city + " , " + country;
+    display_city.innerHTML = city + ", " + country;
 
     xhr.done(function() {
       var api_url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long;
       var api_key = "&APPID=4f730fe861842eaff00d58d1122003ea";
       $.getJSON(api_url + api_key, function(weather) {
-        var weather_city = weather.main.temp;
-        document.getElementById("weather_city_display").innerHTML = weather_city;
+        var farenheit_temp = Math.floor(1.8 * (weather.main.temp - 273) + 32);
+        var current_wind = Math.floor(2.237 * weather.wind.speed);
+        document.getElementById("weather_city_display").innerHTML = farenheit_temp;
+        document.getElementById("wind_display").innerHTML = current_wind;
       });
     })
   })
