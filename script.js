63769f4c0f6d432cb3cf860915923edd54a325ec
kyleroden <kyleroden77@gmail.com>
2017-01-14 10:48:52 -0800

    improved timestamps for the first two forecasts (am/pm and isolating the hours from timestamp)

--- script.js
+++ script.js
@@ -78,15 +78,40 @@ function forecastWeather() {
           //3hr forecast
           const h3_forecast = Math.floor(1.8 * (forecast_data.list[0].main.temp - 273) + 32) + " F";
           const h3_cel_temp = Math.floor(forecast_data.list[0].main.temp - 273.15) + " C";
-          const tomorrow_forecast = forecast_data.list[0].weather[0].description;
-          const tomorrow_icon = forecast_data.list[0].weather[0].icon;
-          const tomorrow_timestamp = forecast_data.list[0].dt_txt;
-          const tomorrow_time = tomorrow_timestamp.slice(5,-3);
+          const first_forecast = forecast_data.list[0].weather[0].description;
+          const first_forecast_icon = forecast_data.list[0].weather[0].icon;
+          const first_forecast_timestamp = forecast_data.list[0].dt_txt;
+          const first_forecast_time = first_forecast_timestamp.slice(5,-3);
+          let first_forecast_hrs = first_forecast_time.slice(6,8);//hour (2 digits)
+          //convert from military /unix time
+          if(first_forecast_hrs > 12) {
+            first_forecast_hrs = first_forecast_hrs % 12;//
+            let first_hrs_str = first_forecast_hrs.toString();
+            //first_hrs_str.trim();
+            first_hrs_str += " PM";
+            document.getElementById("first_forecast_time").innerHTML = "<p class='small'>" + first_hrs_str + "</p>";
+          }
+
           //24hr forecast
           const h24_forecast = Math.floor(1.8 * (forecast_data.list[4].main.temp - 273) + 32) + " F";
           const h24_cel_temp = Math.floor(forecast_data.list[4].main.temp - 273.15) + " C";
           const h24_timestamp = forecast_data.list[4].dt_txt;
+          console.log(h24_timestamp, "h24_timestamp");
           const h24_time = h24_timestamp.slice(5,-3);
+          let fifth_forecast_hrs = h24_time.slice(6,8);
+          console.log("fifth_forecast_hrs", fifth_forecast_hrs)
+          console.log("h24_time: ", h24_time);
+          let fifth_forecast_div = document.getElementById("fifth_forecast_time");
+          if (fifth_forecast_hrs > 12) {
+            fifth_forecast_hrs = fifth_forecast_hrs % 12;
+            let fifth_hrs_str = fifth_forecast_hrs.toString();
+            fifth_hrs_str += " PM";
+            fifth_forecast_div.innerHTML = "<p class='small'>" + fifth_hrs_str + "</p>";
+          } else { //the time should be AM
+            let fifth_hrs_str = fifth_forecast_hrs.toString();
+            fifth_hrs_str += " AM";
+            fifth_forecast_div.innerHTML = "<p class='small'>" + fifth_hrs_str + "</p>";
+          }
           const h24_forecast_description = forecast_data.list[4].weather[0].description;
           const h24_icon = forecast_data.list[4].weather[0].icon;
           //48hr forecast
@@ -98,14 +123,14 @@ function forecastWeather() {
           const h48_icon = forecast_data.list[6].weather[0].icon;
           //write 3hr data
           document.getElementById("3hr_temperature_forecast").innerHTML = "<p class='lead temp_text'>" + h3_forecast + "</p>";
-          document.getElementById("3hr_icon_container").innerHTML = "<img src='http://openweathermap.org/img/w/" + tomorrow_icon + ".png'>";
-          document.getElementById("3hr_forecast_description").innerHTML = "<p class='lead'>" + tomorrow_forecast + "</p>";
-          document.getElementById("first_forecast_time").innerHTML = "<p class='small'>" + tomorrow_time + "</p>";
+          document.getElementById("3hr_icon_container").innerHTML = "<img src='http://openweathermap.org/img/w/" + first_forecast_icon + ".png'>";
+          document.getElementById("3hr_forecast_description").innerHTML = "<p class='lead'>" + first_forecast + "</p>";
+          //document.getElementById("first_forecast_time").innerHTML = "<p class='small'>" + first_forecast_time + "</p>";
           document.getElementById("3hr_celsius_temperature").innerHTML = "<p class='lead temp_text'>" + h3_cel_temp + "</p>";
           //write 24hr data
           document.getElementById("24hr_temperature_forecast").innerHTML = "<p class='lead temp_text'>" + h24_forecast + "</p>";
           document.getElementById("24hr_icon_container").innerHTML = "<img src='http://openweathermap.org/img/w/" + h24_icon + ".png'>";
-          document.getElementById("second_forecast_time").innerHTML = "<p class='small'>" + h24_time + "</p>";
+          //document.getElementById("fifth_forecast_time").innerHTML = "<p class='small'>" + h24_time + "</p>";
           document.getElementById("tomorrow_forecast_description").innerHTML = "<p class='lead'>" + h24_forecast_description + "</p>";
           document.getElementById("24hr_celsius_temperature").innerHTML = "<p class='lead temp_text'>" + h24_cel_temp + "</p>";
           //write 48hr data
